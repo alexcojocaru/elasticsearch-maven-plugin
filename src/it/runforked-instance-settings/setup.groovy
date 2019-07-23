fea5d2237769c8c066012374a1cd3535a50f0dbd
alexcojocaru <alexcojocaru@gmail.com>
2019-07-22 21:49:14 -0700

    lower the IT resource requirements

--- src/it/runforked-instance-settings/setup.groovy
+++ src/it/runforked-instance-settings/setup.groovy
@@ -1,6 +1,6 @@
 import com.github.alexcojocaru.mojo.elasticsearch.v2.ItSetup
 
-def instanceCount = 3
+def instanceCount = 2
 
 // since I cannot pass the props to the maven process directly, save them to the props file;
 // the file is then loaded by the invoker plugin and all props defined in it are set as system props
--- src/it/runforked-instance-settings/src/test/java/com/github/alexcojocaru/mojo/elasticsearch/v2/InstanceSettingsTest.java
+++ src/it/runforked-instance-settings/src/test/java/com/github/alexcojocaru/mojo/elasticsearch/v2/InstanceSettingsTest.java
@@ -25,7 +25,7 @@ public class InstanceSettingsTest extends ItBase
     public void testInstanceSettings() throws ElasticsearchClientException
     {
         final Map nodes = (Map)client.get("/_nodes/settings", HashMap.class).get("nodes");
-        Assert.assertEquals(3, nodes.size());
+        Assert.assertEquals(2, nodes.size());
         final Set<String> nodeNames = new HashSet<String>();
         for (final Object nodeObj : nodes.values()) {
             final Map node = (Map)nodeObj;
@@ -34,13 +34,10 @@ public class InstanceSettingsTest extends ItBase
             final Map attributes = (Map)node.get("attributes");
             if ("first".equals(name)) {
                 Assert.assertEquals("hot", attributes.get("data_type"));
-            } else if ("second".equals(name)) {
-                Assert.assertEquals("warm", attributes.get("data_type"));
             } else {
                 Assert.assertNull(attributes);
             }
         }
         Assert.assertTrue(nodeNames.contains("first"));
-        Assert.assertTrue(nodeNames.contains("second"));
     }
 }
