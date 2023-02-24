c4a30ae964e86ec89e82160abf4d04a6deeda3fc
alexcojocaru <alexcojocaru@users.noreply.github.com>
2023-02-23 22:30:16 -0800

    Issue #151: fix the license conflict (take 2)

--- src/main/java/com/github/alexcojocaru/mojo/elasticsearch/v2/util/ArchiveUtil.java
+++ src/main/java/com/github/alexcojocaru/mojo/elasticsearch/v2/util/ArchiveUtil.java
@@ -1,16 +1,35 @@
 package com.github.alexcojocaru.mojo.elasticsearch.v2.util;
 
 import java.io.File;
-import java.io.IOException;
 
-import org.rauschig.jarchivelib.ArchiverFactory;
+import org.codehaus.plexus.archiver.AbstractUnArchiver;
+import org.codehaus.plexus.archiver.tar.TarGZipUnArchiver;
+import org.codehaus.plexus.archiver.zip.ZipUnArchiver;
 
 public class ArchiveUtil
 {
 
-    public static void extract(File archiveFile, File targetDir) throws IOException
+    public static void extract(File archiveFile, File targetDir)
     {
-        ArchiverFactory.createArchiver(archiveFile).extract(archiveFile, targetDir);
+        AbstractUnArchiver unArchiver;
+
+        String filename = archiveFile.getName().toLowerCase();
+        if (filename.endsWith(".zip"))
+        {
+            unArchiver = new ZipUnArchiver(archiveFile);
+        }
+        else if (filename.endsWith(".tar.gz"))
+        {
+            unArchiver = new TarGZipUnArchiver(archiveFile);
+        }
+        else
+        {
+            throw new IllegalArgumentException(
+                    "Unknown archive type for file: " + archiveFile.getPath());
+        }
+
+        unArchiver.setDestDirectory(targetDir);
+        unArchiver.extract();
     }
 
 }
