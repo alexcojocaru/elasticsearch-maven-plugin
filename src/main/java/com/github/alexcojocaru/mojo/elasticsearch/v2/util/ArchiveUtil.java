5d330bcfe4472a0b5b5d9ffc6e67dd296b717076
alexcojocaru <alexcojocaru@users.noreply.github.com>
2023-02-26 21:50:58 +0000

    Issue 151: fix the license conflict (#153)
    * Issue 151: fix the license conflict
    
    * Issue #151: fix tar.gz unarchiver usage
    
    * Issue #151: attempt to fix the build on Windows

--- src/main/java/com/github/alexcojocaru/mojo/elasticsearch/v2/util/ArchiveUtil.java
+++ src/main/java/com/github/alexcojocaru/mojo/elasticsearch/v2/util/ArchiveUtil.java
@@ -1,35 +1,32 @@
 package com.github.alexcojocaru.mojo.elasticsearch.v2.util;
 
 import java.io.File;
+import java.util.Locale;
 
-import org.codehaus.plexus.archiver.AbstractUnArchiver;
 import org.codehaus.plexus.archiver.tar.TarGZipUnArchiver;
-import org.codehaus.plexus.archiver.zip.ZipUnArchiver;
+import org.zeroturnaround.zip.ZipUtil;
 
 public class ArchiveUtil
 {
 
     public static void extract(File archiveFile, File targetDir)
     {
-        AbstractUnArchiver unArchiver;
-
-        String filename = archiveFile.getName().toLowerCase();
+        String filename = archiveFile.getPath().toLowerCase(Locale.ROOT);
         if (filename.endsWith(".zip"))
         {
-            unArchiver = new ZipUnArchiver(archiveFile);
+            ZipUtil.unpack(archiveFile, targetDir);
         }
         else if (filename.endsWith(".tar.gz"))
         {
-            unArchiver = new TarGZipUnArchiver(archiveFile);
+            TarGZipUnArchiver unArchiver = new TarGZipUnArchiver();
+            unArchiver.setDestDirectory(targetDir);
+            unArchiver.extract(archiveFile.getPath(), targetDir); // the 2nd arg is not used :-o
         }
         else
         {
             throw new IllegalArgumentException(
-                    "Unknown archive type for file: " + archiveFile.getPath());
+                    "Unknown archive type for file name: " + archiveFile.getName());
         }
-
-        unArchiver.setDestDirectory(targetDir);
-        unArchiver.extract();
     }
 
 }
